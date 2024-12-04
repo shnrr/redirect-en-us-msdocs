477065fac0843bd5da3c4a8840b9a69ef0c60df9
Shohei Narita <shnarita@microsoft.com>
2024-12-04 16:56:22 +0900

    241204learning

--- main.user.js
+++ main.user.js
@@ -13,28 +13,28 @@
 // ==/UserScript==
 
 (function () {
-  //https://docs.microsoft.com/en-us/hoge
-  //$1 = docs.microsoft.com, $2=hoge
-  const msDocsUrlReg = new RegExp('https://(.+?)/\\w{2}-\\w{2}/(.*)', 'ig');
-  document.onkeydown = function (e) {
-    if (e.keyCode == 39) {
-      // 右カーソル キーをクリックで en-us に切り替え
-      const currentUrl = location.href; // 現在のページのURLを取得
-      if (currentUrl.match(msDocsUrlReg)) {
-        const nextPage = currentUrl.replace(msDocsUrlReg, "https://$1/en-us/$2"); // マッチさせた文字列以外を置換
-        console.log({"nextPage" : nextPage});
+  // https://docs.microsoft.com/en-us/hoge
+  // $1 = docs.microsoft.com, $2=hoge
+  const msDocsUrlReg = /https:\/\/(.+?)\/\w{2}-\w{2}\/(.*)/i;
+  const docsUrlPattern = /https:\/\/([^\/]+)\/(\w{2}-\w{2})\/(.+)/i;
+
+  document.addEventListener('keydown', async function (e) {
+    const currentUrl = location.href; // 現在のページのURLを取得
+
+    if (e.key === 'ArrowRight') { // 右カーソルキーで en-us に切り替え
+      if (currentUrl.match(msDocsUrlReg) && !currentUrl.includes('/en-us/')) {
+        const nextPage = currentUrl.replace(msDocsUrlReg, "https://$1/en-us/$2");
+        console.log({ nextPage });
         location.href = nextPage;
       }
     }
-    if (e.keyCode == 37) {
-      // 左カーソル キーをクリックで ja-jp に切り替え
-      const currentUrl = location.href; // 現在のページのURLを取得
-      if (currentUrl.match(msDocsUrlReg)) {
-        const nextPage = currentUrl.replace(msDocsUrlReg, "https://$1/ja-jp/$2"); // マッチさせた文字列以外を置換
-        console.log({"nextPage" : nextPage});
+
+    if (e.key === 'ArrowLeft') { // 左カーソルキーで ja-jp に切り替え
+      if (currentUrl.match(msDocsUrlReg) && !currentUrl.includes('/ja-jp/')) {
+        const nextPage = currentUrl.replace(msDocsUrlReg, "https://$1/ja-jp/$2");
+        console.log({ nextPage });
         location.href = nextPage;
       }
     }
-  };
-  
+  });
 })();
